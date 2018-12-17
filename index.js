1133fae43bdffa18972b6b8088577241ed195052
nicole <mailtonicoles@yahoo.com>
2018-12-17 21:00:25 +1100

    Few middlewares have added!
    - Body Parser
    - Cookie Parser
    - Morgan
    - Helmet

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 const PORT = 4000;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => res.send("Hello from Sydney");
+const handleHome = (req, res) => res.send("Hello from Sydney hehehe");
 const handleProfile = (req, res) => res.send("this is a profile page");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(helmet());
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
-
 app.listen(PORT, handleListening);
