6d404cf02e7bf73de4bb73a01fb6e96e7cba5934
nicole <mailtonicoles@yahoo.com>
2018-12-20 20:58:09 +1100

    Set middleware script and updated locals stuff.

--- app.js
+++ app.js
@@ -3,18 +3,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { localsMiddleare } from "./middleware";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import routes from "./routes";
+
 const app = express();
 
+app.use(helmet());
 app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
-app.use(helmet());
+app.use(localsMiddleare);
 
 app.use(routes.home, globalRouter); // join, login, search, home
 app.use(routes.users, userRouter);
