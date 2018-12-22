58c0b468de6d1e1dc226b5ae3562233e35be211a
nicole <mailtonicoles@yahoo.com>
2018-12-22 23:48:55 +1100

    Id & mockup user authentication
    - Set things up which are executed with the user id.
    - Set mockup user authentication data to check what user can see when they logged in. -- will change it later with the real data with MongoDB

--- middleware.js
+++ middleware.js
@@ -3,5 +3,9 @@ import routes from "./routes";
 export const localsMiddleare = (req, res, next) => {
     res.locals.siteName = "Realty Stocks by Nicole Son";
     res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated: true,
+        id: 1
+    };
     next();
 };
