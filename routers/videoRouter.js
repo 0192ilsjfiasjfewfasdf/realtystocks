7bea6df2d3bf2908aca11d32df10856e85a59e69
nicole <mailtonicoles@yahoo.com>
2018-12-25 20:17:36 +1100

    Delete video function is done!

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -16,6 +16,6 @@ videoRouter.get(routes.editVideo(), getEditVideo);
 videoRouter.post(routes.editVideo(), postEditVideo);
 
 //Video Delete
-videoRouter.get(routes.deleteVideo, deleteVideo);
+videoRouter.get(routes.deleteVideo(), deleteVideo);
 
 export default videoRouter;
