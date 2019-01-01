6782ed7d2eb26d99a6ebf40336e39d87bf423a02
nicole <mailtonicoles@yahoo.com>
2019-01-02 00:31:28 +1100

    Styling for footer and login parts have finished
    - Changed the condition of temporary user authentication in middleware.js
    - The scss update from nodemon's side has ignored.
    - Set the color code as same as that of google and fb.

--- middleware.js
+++ middleware.js
@@ -1,16 +1,16 @@
-import routes from "./routes";
 import multer from "multer";
+import routes from "./routes";
 
 const multerVideo = multer({ dest: "uploads/videos/" });
 
 export const localsMiddleare = (req, res, next) => {
-    res.locals.siteName = "Realty Stocks by Nicole Son";
-    res.locals.routes = routes;
-    res.locals.user = {
-        isAuthenticated: true,
-        id: 1
-    };
-    next();
+  res.locals.siteName = "Realty Stocks by Nicole Son";
+  res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: false,
+    id: 1
+  };
+  next();
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
